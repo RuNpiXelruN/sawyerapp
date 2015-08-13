e34374295cb4b5c1acf6f547e304db9c23fb1dc6
runpixelrun <justindavidson23@gmail.com>
2015-08-13 03:06:22 +0000

    changed possible contact_mailer validates syntax error

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default_to: 'justindavidson23@gmail.com'
+    default_to 'justindavidson23@gmail.com'
     
     def contact_email(contact)
         @contact = contact
